859088411a13bcd721a4bc73e02acb4c39ff13b5
valdman40 <valdman40@gmail.com>
2020-05-25 11:32:30 +0300

    getFlights

--- FlightControlWeb/Models/MyFlightManager.cs
+++ FlightControlWeb/Models/MyFlightManager.cs
@@ -8,6 +8,7 @@ using System.Diagnostics;
 using FlightControlWeb.Types;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace FlightControlWeb.Models
 {
@@ -17,24 +18,85 @@ namespace FlightControlWeb.Models
         {
             using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
             {
-                string query = "SELECT * FROM FlightPlan WHERE date_time = '" + date + "' AND is_external = 0";
-                var x = cnn.Query(query, new DynamicParameters());
-                var y = x.ToList();
-                int len = (int)y.Count();
+                string query = "SELECT * FROM FlightPlans";
+                var rowsFromFlightPlans = cnn.Query(query, new DynamicParameters()).ToList();
                 List<Flight> output = new List<Flight>();
-                for (int i = 0; i < len; i++)
+                DateTime dt = DateTime.Parse(date);
+                foreach (var row in rowsFromFlightPlans)
                 {
-                    output.Add(new Flight()
+                    Initial_Location location = getLocation(dt, row.ID, (int)row.Initial_Location_ID, cnn);
+                    if(location != null)
                     {
-                        flight_id = y[i].ID,
-                        company_name = y[i].Company,
-                        date_time = DateTime.Parse(date),
-                        is_external = false
-                    });
+                        output.Add(new Flight()
+                        {
+                            flight_id = row.ID,
+                            latitude = location.latitude,
+                            longitude = location.longitude,
+                            passengers = (int)row.Passengers,
+                            company_name = row.Company,
+                            date_time = dt,
+                            is_external = false
+                        });
+
+                    }
+                    
                 }
                 return output;
             }
         }
+
+        private Initial_Location getLocation(DateTime giventime ,string flightPlan_ID, int initialLocation_ID, IDbConnection cnn)
+        {
+            string SegmentsQuery = "SELECT * FROM Segments WHERE Flight_ID = '" + flightPlan_ID+ "'";
+            string InitialLocQuery = "SELECT * FROM Initial_Locations WHERE ID = " + initialLocation_ID;
+            var rowsFromSegments = cnn.Query(SegmentsQuery, new DynamicParameters()).ToList();
+            var InitialLoc= cnn.Query(InitialLocQuery, new DynamicParameters()).ToList()[0];
+            List<Segment> segments = new List<Segment>();
+            foreach (var row in rowsFromSegments)
+            {
+                segments.Add(new Segment()
+                {
+                    latitude = row.Latitude,
+                    longitude = row.Longitude,
+                    timespan_seconds = (int)row.Timespan
+                }); 
+            }
+
+            DateTime departtime = DateTime.Parse(InitialLoc.Date);
+            Segment startSegment = new Segment()
+            {
+                longitude = InitialLoc.Longitude,
+                latitude = InitialLoc.Latitude,
+                timespan_seconds = 0
+            };
+            DateTime landtime = DateTime.Parse(InitialLoc.Date);
+            foreach(Segment endSegment in segments)
+            {
+                   landtime = landtime.AddSeconds(endSegment.timespan_seconds);
+                // check if giventime is in range
+                if (giventime.CompareTo(departtime) >= 0 && giventime.CompareTo(landtime) <= 0)
+                {
+                    TimeSpan secondsOnAir = giventime.Subtract(departtime);
+                    double progress = secondsOnAir.TotalSeconds / endSegment.timespan_seconds; // between 0-1
+                    Line line = new Line();
+                    // x is longitude, y is latitude
+                    line.start = new Point() { X = startSegment.longitude, Y = startSegment.latitude };
+                    line.end = new Point() { X = endSegment.longitude, Y = endSegment.latitude };
+                    Point onSegment = line.getPointOnSegment(progress);
+                    return new Initial_Location()
+                    {
+                        latitude = onSegment.Y,
+                        longitude = onSegment.X,
+                        date_time = giventime
+                    };
+                }
+                departtime = landtime; // try with next round
+                startSegment = endSegment;
+            }
+
+            return null;
+        }
+
         public  List<Flight> GetAllFlights(string date)
         {
             using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
---
+++ FlightControlWeb/Types/Line.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightControlWeb.Types
+{
+    public class Line
+    {
+        public Point start { get; set; }
+        public Point end { get; set; }
+        public Point getPointOnSegment(double partial)
+        {
+            Point returnVal = new Point();
+            returnVal.X = start.X + partial * (end.X - start.X);
+            returnVal.Y = start.Y + partial * (end.Y - start.Y);
+            return returnVal;
+        }
+
+
+    }
+}
