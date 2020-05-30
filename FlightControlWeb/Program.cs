db519f8351d934db715de60c300075bfb99be95c
edenhigani <edenh018@gmail.com>
2020-05-30 15:54:18 +0300

    test

--- FlightControlWeb/Program.cs
+++ FlightControlWeb/Program.cs
@@ -14,6 +14,7 @@ namespace FlightControlWeb
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            Console.WriteLine("d");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
